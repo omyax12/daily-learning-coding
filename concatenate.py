0f704b97a38c60a61c6be02364ba0f3074179c90
Omkar Sanap <274878275+omyax12@users.noreply.github.com>
2026-08-16 21:35:16 +0530

    Add files via upload

---
+++ concatenate.py
@@ -0,0 +1,8 @@
+#take input from user
+first_name=input("Enter first name: ")
+middle_name=input("Enter middle name: ")
+last_name=input("Enter last name: ")
+#now arrange the name
+full_name=first_name +" "+ middle_name +" "+ last_name
+#print full name 
+print("Full Name is : ",full_name)
